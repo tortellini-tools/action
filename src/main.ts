0d318398bda92347af13942efc0815cdf50a9dc0
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-05-18 18:14:43 +0200

    linter

--- src/main.ts
+++ src/main.ts
@@ -17,43 +17,44 @@
 // }
 //
 
-import * as shell from '@actions/exec';
-
+import * as shell from '@actions/exec'
 
 class Ortolan {
-    private owner: string;
-    private repo: string;
+    private owner: string
+    private repo: string
 
     constructor(owner: string, repo: string) {
-        this.owner = owner;
-        this.repo = repo;
+        this.owner = owner
+        this.repo = repo
     }
 
-    analyze (): void {
-        const cwd = process.cwd();
-        const bindMountInput = `${cwd}/in/${this.owner}/${this.repo}:/project`;
-        const bindMountOutput = `${cwd}/out/${this.owner}/${this.repo}:/out`;
-        shell.exec(`docker run --rm -v ${bindMountInput} -v ${bindMountOutput} ort analyze -i /project -o /out`)
+    analyze(): void {
+        const cwd = process.cwd()
+        const bindMountInput = `${cwd}/in/${this.owner}/${this.repo}:/project`
+        const bindMountOutput = `${cwd}/out/${this.owner}/${this.repo}:/out`
+        shell.exec(
+            `docker run --rm -v ${bindMountInput} -v ${bindMountOutput} ort analyze -i /project -o /out`
+        )
     }
 
-    clone (): void {
-        shell.exec(`git clone https://github.com/${this.owner}/${this.repo} in/${this.owner}/${this.repo}`);
+    clone(): void {
+        shell.exec(
+            `git clone https://github.com/${this.owner}/${this.repo} in/${this.owner}/${this.repo}`
+        )
     }
 
-    run (): void {
-        this.clone();
-        this.analyze();
+    run(): void {
+        this.clone()
+        this.analyze()
     }
 }
 
 const repositories = [
-    {owner: "iomega", repo: "zenodo-upload"},
-    {owner: "xenon-middleware", repo: "xenon-cli"}
+    {owner: 'iomega', repo: 'zenodo-upload'},
+    {owner: 'xenon-middleware', repo: 'xenon-cli'}
 ]
 
-repositories.forEach((repository):void => {
-    const {owner, repo} = repository;
-    const ortolan = new Ortolan(owner, repo);
-    ortolan.run();
-})
-
+for (const {owner, repo} of repositories) {
+    const ortolan = new Ortolan(owner, repo)
+    ortolan.run()
+}
