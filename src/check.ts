5085fec05772efecd37223586665aba44ef5523e
Faruk D <f.diblen@esciencecenter.nl>
2021-05-31 17:30:55 +0200

    clean up:

--- src/check.ts
+++ src/check.ts
@@ -23,40 +23,21 @@ export async function check_urls(
         // split the contents by new line
         const url_list: string[] = url_data.split(/\r?\n/)
 
-        // // iterate over list of urls, clone and run analyze
-        // for (const repo_url of Object.values(url_list)) {
-        //     console.log(repo_url)
-        //     // const {owner, repo} = get_owner_and_repo(repo_url)
-        //     // run_git_clone(repo_url) // dest folder should define the folder
-        //     // await analyze(repo_dir, output_dir) //run analyze
-        // }
-
+        // get repo owner and repo name
         const repo_info: GitRepo[] = url_list.map(get_owner_and_repo)
 
-        // for (const repo_detail of Object.values(repo_info)) {
-        //     console.log(repo_detail)
-        //     const clone_path = repo_detail.owner.concat('/', repo_detail.repo)
-        //     await run_git_clone(url_list[0], clone_path)
-        //     await analyze(repo_dir, output_dir)
-        // }
-
+        // clone each repo and run analyze
         for (const [index, val] of repo_info.entries()) {
-            // your code goes here
-            console.log(index)
-            console.log(val)
             const clone_path = clone_dir.concat(
                 repo_info[index].owner,
                 '/',
                 repo_info[index].repo
             )
-            console.log(clone_path)
             const analyze_path = output_dir.concat('/', clone_path)
-            console.log(analyze_path)
             await run_git_clone(url_list[index], clone_path)
             await analyze(clone_path, analyze_path)
         }
 
-        // console.log(repo_info)
     } catch (err) {
         console.error(err)
     }
