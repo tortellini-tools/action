1058081adac5cdc2bfcb1f6915c4c146dd379ea2
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-06-09 09:20:52 +0200

    Added types

--- src/stats.ts
+++ src/stats.ts
@@ -3,6 +3,21 @@ import {parse} from 'node-html-parser'
 import atob from 'atob'
 import * as pako from 'pako'
 
+export interface RepoStats {
+    files: number
+    dependencies: number
+    issues: number
+    licenses: number
+    repo: string // GH repo name
+    owner: string // GH organization or user
+    repo_url: string // url of repository
+    revision: string
+    report: string // path to web app file
+    scopes: number
+    dependency_levels: number
+    violations: number
+}
+
 export async function collect_stats(webapp_file: string): Promise<void> {
     const webapp_content = await fs.promises.readFile(webapp_file, {
         encoding: 'utf-8'
@@ -12,13 +27,21 @@ export async function collect_stats(webapp_file: string): Promise<void> {
     // return evaluated_model2stats(evaluated_model)
 }
 
-export function parse_html(content: string) {
+export function parse_html(content: string): string {
     const root = parse(content)
     const element = root.querySelector('#ort-report-data')
     return element.textContent.trim()
 }
 
-export function decode_blob(blob: string) {
+interface Issue {
+    message: string
+}
+
+interface EvaluatedModel {
+    issues: Issue[]
+}
+
+export function decode_blob(blob: string): EvaluatedModel {
     // Copied from https://github.com/oss-review-toolkit/ort/blob/bd2494938a82843932738780ce27707900976334/reporter-web-app/src/sagas/index.js#L40-L51
 
     // Decode Base64 (convert ASCII to binary).
